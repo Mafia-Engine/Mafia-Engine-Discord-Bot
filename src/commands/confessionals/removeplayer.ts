22f69f27b85104a63e470e858af0a4a096256a93
Jack Nash <jacknash42@gmail.com>
2022-08-14 07:30:31 +1000

    add remove command for player chats

--- src/commands/confessionals/removeplayer.ts
+++ src/commands/confessionals/removeplayer.ts
@@ -1,27 +1,43 @@
-import { CategoryChannel, CommandInteraction, TextChannel} from "discord.js";
-import { ConfessionalsRaw, ConfessionalsSchema, IndividualConfessional, updateChannelPermissions } from "../../database/Confessionals";
-import { SlashCommand } from "../../structures/SlashCommand";
+import { CategoryChannel, CommandInteraction, TextChannel } from 'discord.js';
+import { ConfessionalsRaw, ConfessionalsSchema, IndividualConfessional, updateChannelPermissions } from '../../database/Confessionals';
+import { SlashCommand } from '../../structures/SlashCommand';
 
 export const slashCommand: SlashCommand = {
-    name: 'remove',
-    description: '[HOST] Remove a player/chat.',
-    commandData: [
-        {
-            name: 'channel',
-            description: 'Remove a player based from their chat channel (will delete the channel)',
-            type: 'CHANNEL',
-            required: true
-        },
-        {
-            name: 'user',
-            description: 'Remove a player based from their account (will delete the channel)',
-            type: 'STRING',
-            required: false
-        }
-    ],
+	name: 'remove',
+	description: '[HOST] Delete a player chat',
+	commandData: [
+		{
+			name: 'channel',
+			description: 'Remove a player based from their chat channel (will delete the channel)',
+			type: 'CHANNEL',
+			required: true,
+		},
+	],
 
-    commandFunction: async (i: CommandInteraction) => {
-        await i.deferReply().catch(console.log);
-        await i.editReply('This command has not been fully made yet').catch(console.log);
-    }
-}
+	commandFunction: async (i: CommandInteraction) => {
+		await i.deferReply().catch(console.log);
+		const channel = i.channel as TextChannel;
+		if (channel.name !== 'host-panel') return i.editReply('You cannot use this command outside of the dedicated host panel.').catch(console.log);
+
+		const reqChannel = i.options.getChannel('channel', true);
+
+		try {
+			const fetchedConfessional = await ConfessionalsSchema.findOne({ hostPanelId: channel.parentId });
+			if (!fetchedConfessional) return await i.editReply('Cannot find a stored player chats linked to this category.').catch(console.log);
+
+			let deleteFromIndex: number | null = null;
+			if (fetchedConfessional.confessionals) {
+				fetchedConfessional.confessionals.forEach((val: IndividualConfessional, index: number) => {
+					if (val.channelId === reqChannel.id) deleteFromIndex = index;
+				});
+
+				if (deleteFromIndex) fetchedConfessional.confessionals.splice(deleteFromIndex, 1);
+			}
+
+			await fetchedConfessional.save();
+		} catch (err) {
+			console.log(err);
+			await i.editReply(`An unexpected error has occurred.`).catch(console.log);
+		}
+	},
+};
--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,30 @@
 import dotenv from 'dotenv';
-import {  Client, Intents } from 'discord.js';
+import { Client, Intents } from 'discord.js';
 import { loadCommands, loadListeners } from './structures/SlashCommand';
-import axios from 'axios'
+import axios from 'axios';
 import mongoose from 'mongoose';
 
-axios.defaults.baseURL = 'http://localhost:3001/v1/'
+axios.defaults.baseURL = 'http://localhost:3001/v1/';
 
 dotenv.config();
 const { discordToken } = process.env;
 const SERVER_ID_CORE = '648663810772697089';
-const SERVER_ID_CONFESSIONALS = '990547469203038238'
-const SERVER_ID_PLAYER_CHATS = '753231987589906483'
+const SERVER_ID_CONFESSIONALS = '990547469203038238';
+const SERVER_ID_PLAYER_CHATS = '753231987589906483';
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES] });
 client.on('ready', async () => {
 	if (!process.env.CORE_DATABASE) console.log('Database URI not supplied.');
-	else mongoose.connect(process.env.CORE_DATABASE).then(()=>console.log('DB Connected')).catch((e) => console.log('DB not connected', e));
+	else
+		mongoose
+			.connect(process.env.CORE_DATABASE)
+			.then(() => console.log('DB Connected'))
+			.catch((e) => console.log('DB not connected', e));
 
-	loadCommands(client, SERVER_ID_CORE, 'core')
+	loadCommands(client, SERVER_ID_CORE, 'core');
 	loadCommands(client, SERVER_ID_CONFESSIONALS, 'confessionals');
 	loadCommands(client, SERVER_ID_PLAYER_CHATS, 'confessionals');
 	loadListeners(client);
-	
 });
 
 client.login(discordToken);
