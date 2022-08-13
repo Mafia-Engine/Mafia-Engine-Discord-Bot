e9cc068c57b825084f8075e0f81081c80a485df3
Jack Nash <jacknash42@gmail.com>
2022-08-14 08:05:16 +1000

    debug player chat remove command

--- src/commands/confessionals/removeplayer.ts
+++ src/commands/confessionals/removeplayer.ts
@@ -1,18 +1,14 @@
-import { CategoryChannel, CommandInteraction, TextChannel } from 'discord.js';
+import { SelectMenuBuilder } from '@discordjs/builders';
+import { CategoryChannel, CommandInteraction, MessageActionRow, MessageSelectMenu, MessageSelectOptionData, TextChannel } from 'discord.js';
 import { ConfessionalsRaw, ConfessionalsSchema, IndividualConfessional, updateChannelPermissions } from '../../database/Confessionals';
 import { SlashCommand } from '../../structures/SlashCommand';
 
+import { UnsafeSelectMenuOptionBuilder } from '@discordjs/builders';
+
 export const slashCommand: SlashCommand = {
 	name: 'remove',
-	description: '[HOST] Delete a player chat',
-	commandData: [
-		{
-			name: 'channel',
-			description: 'Remove a player based from their chat channel (will delete the channel)',
-			type: 'CHANNEL',
-			required: true,
-		},
-	],
+	description: '[HOST] Delete a player-chat',
+	commandData: [],
 
 	commandFunction: async (i: CommandInteraction) => {
 		await i.deferReply().catch(console.log);
@@ -20,25 +16,28 @@ export const slashCommand: SlashCommand = {
 		if (channel.name !== 'host-panel') return i.editReply('You cannot use this command outside of the dedicated host panel.').catch(console.log);
 
 		try {
-			const reqChannel = i.options.getChannel('channel', true) as TextChannel;
-
 			const fetchedConfessional = await ConfessionalsSchema.findOne({ hostPanelId: channel.parentId });
 			if (!fetchedConfessional) return await i.editReply('Cannot find a stored player chats linked to this category.').catch(console.log);
 
-			let deleteFromIndex: number | null = null;
+			let allPlayers: MessageSelectOptionData[] = [];
 			if (fetchedConfessional.confessionals) {
-				fetchedConfessional.confessionals.forEach((val: IndividualConfessional, index: number) => {
-					if (val.channelId === reqChannel.id) deleteFromIndex = index;
-				});
+				for (let i = 0; i < fetchedConfessional.confessionals.length; i++) {
+					const val: IndividualConfessional = fetchedConfessional.confessionals[i];
+					const user = await channel.guild.members.fetch(val.user);
 
-				if (deleteFromIndex) {
-					fetchedConfessional.confessionals.splice(deleteFromIndex, 1);
-					reqChannel.delete();
-					await i.editReply({ content: `Channel has been deleted.` });
-					await fetchedConfessional.save();
-				} else {
-					await i.editReply({ content: `Channel cannot be deleted as it does not exist on the DB` });
+					let username = user.displayName;
+
+					const data: MessageSelectOptionData = {
+						label: username,
+						value: user.id,
+					};
+
+					allPlayers.push(data);
 				}
+
+				const row = new MessageActionRow().addComponents(new MessageSelectMenu().setCustomId('remove-players').setPlaceholder('Select users to remove').setOptions(allPlayers).setMinValues(1));
+
+				await i.editReply({ components: [row] });
 			} else {
 				await i.editReply({ content: `Error fetching player chats from the database` });
 			}
